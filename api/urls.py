4573ab7d15cd4de7ef19674c1db2dec27fe96bb7
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-08-24 13:31:42 +0000

    Api For Update Note Error

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.getRountes, name='rountes'),
-    path('notes', views.NotesList.as_view(), name='note_list'),
-    path('notes/<str:pk>', views.GetNotes.as_view(), name='get_notes'),
-    path('notes/<str:pk>/update', views.UpdateNoteView.as_view(), name='update_note'),
+    path('notes/', views.NotesList.as_view(), name='note_list'),
+    path('notes/<str:pk>/', views.GetNotes.as_view(), name='get_notes'),
+    path('notes/<str:pk>/update/', views.UpdateNoteView.as_view(), name='update_note'),
 
 
 ]
