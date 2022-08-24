e762953e2e21252ba09696afc5174976074d4ecb
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-08-24 15:21:55 +0000

    Delete APi

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('notes/', views.NotesList.as_view(), name='note_list'),
     path('notes/<str:pk>/', views.GetNotes.as_view(), name='get_notes'),
     path('notes/<str:pk>/update/', views.UpdateNoteView.as_view(), name='update_note'),
-
-
+    path('notes/<str:pk>/delete/', views.DeleteNoteView.as_view(), name='delete_note'),
 ]
